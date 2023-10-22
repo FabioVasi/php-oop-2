b9f11cd7f3dc5aa036b6aff44e662d2b92e227dc
FabioVasi <vasifabio96@gmail.com>
2023-10-22 21:25:07 +0200

    add bonus partials end ex

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ Bonus (non opzionale):
 organizzate il progetto come visto stamattina a lezione usando varie sottocartelle per inserire classi, layout e dati.
 */
 
+require __DIR__ . '/Partials/Header.php';
+require __DIR__ . '/Partials/Footer.php';
 require __DIR__ . '/Models/Product.php';
 require __DIR__ . '/Models/alimentation.php';
 require __DIR__ . '/Models/toys.php';
@@ -21,44 +23,29 @@ require __DIR__ . '/db.php';
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oop 2</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body class="bg-secondary">
-
-    <main>
-        <div class="container text-center">
-            <h1 class="text-white fw-bolder">Pet's Shop</h1>
-            <div class="row">
-                <?php foreach ($products as $product) : ?>
-                    <div class="col-6 g-3">
-                        <div class="card bg-dark text-white text-start p-3 text-start">
-                            <div>
-                                <div class="d-flex justify-content-start align-items-center"><?php echo $product->getPetImage(); ?></div>
-                                <h2>Pet Type: <?php echo $product->pet; ?></h2>
-                                <p><?php echo $product->kibble; ?></p>
-                            </div>
-                            <div>
-                                <h3>Price: <?php echo $product->price; ?> €</h3>
-                                <p><?php echo $product->kennel; ?></p>
-                            </div>
-                            <div>
-                                <h3>Product: <?php echo $product->product; ?></h3>
-                                <p><?php echo $product->toy; ?></p>
-                            </div>    
+<main>
+    <div class="container text-center">
+        <h1 class="text-white fw-bolder">Pet's Shop</h1>
+        <div class="row">
+            <?php foreach ($products as $product) : ?>
+                <div class="col-6 g-3">
+                    <div class="card bg-dark text-white text-start p-3 text-start">
+                        <div>
+                            <div class="d-flex justify-content-start align-items-center"><?php echo $product->getPetImage(); ?></div>
+                            <h2>Pet Type: <?php echo $product->pet; ?></h2>
+                            <p><?php echo $product->kibble; ?></p>
                         </div>
+                        <div>
+                            <h3>Price: <?php echo $product->price; ?> €</h3>
+                            <p><?php echo $product->kennel; ?></p>
+                        </div>
+                        <div>
+                            <h3>Product: <?php echo $product->product; ?></h3>
+                            <p><?php echo $product->toy; ?></p>
+                        </div>    
                     </div>
-                <?php endforeach ?>
-            </div>
+                </div>
+            <?php endforeach ?>
         </div>
-    </main>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</script>
-</body>
-</html>
+    </div>
+</main>
